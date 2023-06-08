24b531e415a78ff97dc9dba1686ba86369b11b00
JunKubota <kinkado115@gmail.com>
2023-06-08 12:50:17 +0000

    initial commit

---
+++ launch/urdf_open.launch.py
@@ -0,0 +1,70 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+import launch
+import launch_ros
+
+def generate_launch_description():
+    try:
+        # URDFファイルのパスを取得
+        pkg_share = get_package_share_directory('camera-sensor-example')
+    except FileNotFoundError as e:
+        print(f"PackageNotFoundError: {e}")
+        return LaunchDescription()
+
+    try:
+        urdf_file = os.path.join(pkg_share, 'urdf', 'mobile_robot.urdf')
+        # 以降の処理
+    except FileNotFoundError as e:
+        print(f"FileNotFoundError: {e}")
+        return LaunchDescription()
+    # RViz設定ファイルのパスを取得
+    rviz_config_file = os.path.join(pkg_share, 'rviz', 'defalut.rviz')
+
+    # launchファイルのパラメータ設定
+    use_sim_time = LaunchConfiguration('use_sim_time', default='false')
+
+    # RVizノードの起動
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        output='screen',
+        parameters=[{'use_sim_time': use_sim_time}]
+    )
+
+    # URDFロードノードの起動
+    robot_state_publisher_node = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        name='robot_state_publisher',
+        output='screen',
+        parameters=[{'use_sim_time': use_sim_time}],
+        arguments=[urdf_file]
+    )
+
+    # launchファイルの構成
+    ld = LaunchDescription()
+
+    # launchファイルへの引数の追加
+    ld.add_action(DeclareLaunchArgument(
+        'use_sim_time',
+        default_value=use_sim_time,
+        description='Use simulation/Gazebo clock if true'))
+
+    # ノードの起動
+    ld.add_action(rviz_node)
+    ld.add_action(robot_state_publisher_node)
+
+    return ld
+def main(argv=None):
+    # Create launch description and execute it
+    ld = generate_launch_description()
+    launch.launch(ld)
+
+if __name__ == '__main__':
+    main()
---
+++ setup.py
@@ -0,0 +1,33 @@
+import os
+from glob import glob
+from setuptools import setup
+
+package_name = 'camera-sensor-example'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name), glob('launch/*.launch.py')),
+        (os.path.join('share', package_name, "worlds"), glob('worlds/*.sdf')),
+        (os.path.join('share', package_name, "models", "arashcamera"),
+         glob('models/arashcamera/*')),
+        (os.path.join('share', package_name, "urdf"), glob('urdf/*.urdf')),
+        (os.path.join('share', package_name, "rviz"), glob('rviz/*.rviz')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='arashsm79',
+    maintainer_email='arashsm79@yahoo.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
